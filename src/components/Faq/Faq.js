4cdcae6db0158ac3d57a8841c705bf12f0b2d38c
Alireza <lovelylektus@yahoo.com>
2017-12-08 11:34:43 -0800

    Faq Page added in Project and axios and another things that we need in the Our  faq Page

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,11 +1,42 @@
 import React,{Component}from 'react';
+import './faq.css';
+import axios from 'axios';
 class Faq extends Component {
-    
+    constructor(props){
+        super(props);
+        this.state={
+            faqs: []
+        };
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res =>{
+                const faqs = res.data.slice(0,10);
+                this.setState({faqs});
+            })
+    }
     render() {
         return (
-            <div>
-                    some happned here   
+            <div className="faq">
+        <div className="container">
+        <section className="section">
+            <h1 className="title">FAQ</h1>
+            <h2 className="subtitle is-4">Lorem ipsum and all of that jaaz.</h2>
+            <div className="columns" >
+            {this.state.faqs.map(faq =>
+                <div className="column">
+                <div className="card">
+                <div className="card-content">
+                    <p style={{fontSize:20 , fontWeight:'bold'}}>{faq.title}</p>
+                    <p className="answer">{faq.body}</p>
                 </div>
+                </div>
+            </div>
+            )}
+            </div>
+        </section>
+    </div>
+  </div>
         );
     }
 }
